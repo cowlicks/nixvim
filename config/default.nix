9650ab1b4a91d18f11b529f073e939bc0768c3ed
Blake Griffith <blake.a.griffith@gmail.com>
2024-10-02 15:21:05 -0400

    disable bufferline, enable nixd lsp

--- config/default.nix
+++ config/default.nix
@@ -1,18 +1,19 @@
 {
   # Import all your configuration modules here
-  imports = [ ./bufferline.nix ];
   colorschemes.gruvbox.enable = true;
 
   plugins.lsp = {
+    enable = true;
+    servers = {
+      rust-analyzer = {
+        installCargo = true;
+        installRustc = true;
         enable = true;
-        servers = {
-          rust-analyzer = {
-            installCargo = true;
-            installRustc = true;
-            enable = true;
-
-          };
-        };
       };
+      nixd = {
+        enable = true;
+      };
+    };
+  };
   extraConfigVim = builtins.readFile ./vimrc;
 }
