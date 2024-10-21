56ce1dd8c6d1693f868cddd800809a65f124ccfd
Blake Griffith <blake.a.griffith@gmail.com>
2024-10-21 11:24:41 -0400

    formatted when nix-build was run?

--- config/default.nix
+++ config/default.nix
@@ -1,28 +1,46 @@
 {
   # Import all your configuration modules here
   colorschemes.gruvbox.enable = true;
-
   plugins = {
-  nix.enable = true;
-  lsp-format.enable = true;
-  lsp = {
-    enable = true;
-    servers = {
-      rust-analyzer = {
-        installCargo = true;
-        installRustc = true;
-        enable = true;
+    treesitter.enable = true;
+    cmp = {
+      enable = true;
+      autoEnableSources = true;
+      settings.sources = [
+        { name = "nvim_lsp"; }
+        { name = "path"; }
+        { name = "buffer"; }
+        { name = "utilsnips"; }
+        { name = "tmux"; }
+      ];
+    };
+    cmp-nvim-lsp.enable = true;
+    nix.enable = true;
+    lsp-format.enable = true;
+    lsp = {
+    keymaps = {
+        lspBuf = {
+          gr = "references";
+          gd = "definition";
+        };
       };
-      # should be ts-ls according to
-      # https://nix-community.github.io/nixvim/plugins/lsp/servers/ts-ls/index.html
-      tsserver = {
-        enable = true;
-      };
-      nixd = {
-        enable = true;
+      enable = true;
+      servers = {
+        rust-analyzer = {
+          installCargo = true;
+          installRustc = true;
+          enable = true;
+        };
+        # should be ts-ls according to
+        # https://nix-community.github.io/nixvim/plugins/lsp/servers/ts-ls/index.html
+        tsserver = {
+          enable = true;
+        };
+        nixd = {
+          enable = true;
+        };
       };
     };
-  };
   };
   extraConfigLua = builtins.readFile ./vimrc.lua;
   extraConfigVim = builtins.readFile ./vimrc;
