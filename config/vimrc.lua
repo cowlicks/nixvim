65328312b21fd157c947369c3c00af7e2e4e50f4
Blake Griffith <blake.a.griffith@gmail.com>
2024-11-01 16:07:44 -0400

    ,tc show all commands in telescope

--- config/vimrc.lua
+++ config/vimrc.lua
@@ -1,2 +1,3 @@
 vim.api.nvim_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>ac', '<cmd>lua vim.lsp.buf.code_action()<CR>', {})
+vim.keymap.set('n', '<leader>tc', '<cmd>Telescope commands<CR>')
