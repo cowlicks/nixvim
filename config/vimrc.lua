564b77d07d819b620d79fb8149b9bc04c816c055
Blake Griffith <blake.a.griffith@gmail.com>
2024-11-01 16:08:20 -0400

    better ctrl-w *for windows

--- config/vimrc.lua
+++ config/vimrc.lua
@@ -16,3 +16,20 @@ vim.keymap.set('n', '<leader>bd', function()
 
     vim.api.nvim_buf_delete(current_buf, {})
 end, { noremap = true })
+
+-- Make it so holding down 'ctrl', and pressing 'w' will cycle through windows for every press of 'w' after the first instead of every 2 presses.
+-- Track the timestamp of last <C-w> press
+local last_w_press = 0
+-- Time window for last press to count for cycle window
+local last_w_press_time_limit = 1000;
+vim.keymap.set('n', '<C-w>', function()
+    local now = vim.loop.now()
+    -- If pressed within 200ms, cycle windows
+    if now - last_w_press < last_w_press_time_limit then
+        vim.cmd('wincmd w')
+    else
+        -- Otherwise, act as normal <C-w>
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-w>', true, false, true), 'n', false)
+    end
+    last_w_press = now
+end, { noremap = true })
