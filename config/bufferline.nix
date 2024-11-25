773217e4fb2861c1652153966834188210fae1ad
Blake Griffith <blake.a.griffith@gmail.com>
2024-11-25 10:53:26 -0500

    Add underlines for tabs

--- config/bufferline.nix
+++ config/bufferline.nix
@@ -1,5 +1,31 @@
 {
   plugins.bufferline = {
     enable = true;
+    settings = {
+      highlights = {
+        buffer_selected = {
+          underline = true;
+          undercurl = false;
+          italic = false;
+          sp = "#b8bb26";
+        };
+        buffer_visible = {
+          underline = true;
+          undercurl = false;
+          italic = false;
+          sp = "#d79921";
+        };
+      };
+      options = {
+        separator_style = "thick";
+        show_tab_indicators = true;
+        show_buffer_close_icons = true;
+        show_close_icon = false;
+        color_icons = true;
+        # This ensures the underline spans the full width of the tab
+        #BG: Removed because it prevents tab names from de-duplicating
+        #enforce_regular_tabs = true;
+      };
+    };
   };
 }
